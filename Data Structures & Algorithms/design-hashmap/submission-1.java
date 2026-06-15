09f2ac487db36caa891bd028fe5d8933034fc319
Sid22010134 <85012524+Sid22010134@users.noreply.github.com>
2026-06-15 15:55:14 +0530

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.java
@@ -0,0 +1,20 @@
+class MyHashMap {
+
+    int[] hashMap;
+    public MyHashMap() {
+        hashMap = new int[1000001];
+    }
+    
+    public void put(int key, int value) {
+        hashMap[key] = value+1;
+    }
+    
+    public int get(int key) {
+        return hashMap[key]-1;
+    }
+    
+    public void remove(int key) {
+        hashMap[key] = 0;
+
+    }
+}
